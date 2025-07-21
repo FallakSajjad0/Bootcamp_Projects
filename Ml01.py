ddc83e2b52a165d863d755240b104c2a740cee93
fallak <rajasanikhan6@gmail.com>
2025-07-21 15:26:09 +0500

    no comments

--- Ml01.py
+++ Ml01.py
@@ -3,23 +3,23 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 df = pd.read_csv("iris.csv")
-# df = pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])
-# print(df)
+df = pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])
+print(df)
 
-# df = pd.DataFrame({"names": ['Fallak', 'Ahmad', 'Ali'], 
-#                   "RollNo": [1122, 1123, 1124],
-#                   "city" : ['Havelian', 'Mansehra', 'Mansehra']})
-# print(df)
+df = pd.DataFrame({"names": ['Fallak', 'Ahmad', 'Ali'], 
+                  "RollNo": [1122, 1123, 1124],
+                  "city" : ['Havelian', 'Mansehra', 'Mansehra']})
+print(df)
 
-# def fx(a):
-#     return a*a
-# df['sqrofsepllength'] = df['Sepallengthincm'].apply(fx)
-# df['zeros'] = [i for i in range(len(df))]
+def fx(a):
+    return a*a
+df['sqrofsepllength'] = df['Sepallengthincm'].apply(fx)
+df['zeros'] = [i for i in range(len(df))]
 
-# print(df.head())
-# print(df.info())
+print(df.head())
+print(df.info())
 
-# print(df.to_csv("irise2.csv", index=False))
+print(df.to_csv("irise2.csv", index=False))
 
 a = np.array([84, 85 ,81, 89, 88, 86]
              )
