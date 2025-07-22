30f04e88e9b09b21c0face2864c7171ef706bba0
fallak <rajasanikhan6@gmail.com>
2025-07-22 15:05:01 +0500

    Overall text analysis

--- Simple Sentiment Text Analysis.py
+++ Simple Sentiment Text Analysis.py
@@ -1,16 +1,21 @@
 from textblob import TextBlob
-# from newspaper import Article
+from newspaper import Article
 
-# url = ""
+url = "https://en.wikipedia.org/wiki/Matthew_Muller"
+article = Article(url)
 
-# article = Article(url)
+article.download()
+article.parse()
+article.nlp()
 
-# article.download()
-# article.parse()
-# article.nlp()
+text0 = article.summary
+print(text0)
 
-# text = article.summary
-# print(text)
+print("\n__Through URL__")
+
+blob = TextBlob(text0)
+sentiment0 = blob.sentiment.polarity 
+print(f"\nSentiment : {sentiment0}")
 
 with open('negative.txt', 'r') as n:
     text = n.read()
@@ -18,6 +23,7 @@ with open('negative.txt', 'r') as n:
 with open('positive.txt', 'r') as p:
     text2 = p.read()
 
+print("\n__Through Texts__")
 
 blob = TextBlob(text)
 sentiment = blob.sentiment.polarity # -1 to 1
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
-r
+print(text0)
+
+blob = TextBlob(text0)
+sentiment0 = blob.sentiment.polarity 
+print(f"Sentiment0 : {sentiment0}")
