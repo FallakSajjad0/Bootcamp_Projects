ac4e921cbcc5815abe40b7400a46923eef1baa5c
fallak <rajasanikhan6@gmail.com>
2025-07-23 12:45:35 +0500

    adding comments to program

--- Simple_Sentiment_Text_Analysis.py
+++ Simple_Sentiment_Text_Analysis.py
@@ -4,6 +4,8 @@ from newspaper import Article
 url = "https://en.wikipedia.org/wiki/Muhammad_Ali"
 article = Article(url)
 
+# Loading Data
+
 article.download()
 article.parse()
 article.nlp()
@@ -13,10 +15,12 @@ print(text0)
 
 print("\n__Through URL__")
 
+# Cheaking and showing polarity -1 to 1, Sentiment for data from url
 blob = TextBlob(text0)
 sentiment0 = blob.sentiment.polarity 
 print(f"\nSentiment : {sentiment0}")
 
+# loading self made txt data
 with open('negative.txt', 'r') as n:
     text = n.read()
 
@@ -24,6 +28,8 @@ with open('positive.txt', 'r') as p:
     text2 = p.read()
 
 print("\n__Through Texts__")
+
+# Cheaking and showing polarity -1 to 1, Sentiment for data from txt files
 
 blob = TextBlob(text)
 sentiment = blob.sentiment.polarity # -1 to 1
