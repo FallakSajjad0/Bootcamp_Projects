b78a0b6a28d987408979ec0037cfd5f348c9f2be
fallak <rajasanikhan6@gmail.com>
2025-07-23 14:46:59 +0500

    Comments Added

--- Simple_Sentiment_Text_Analysis.py
+++ Simple_Sentiment_Text_Analysis.py
@@ -1,10 +1,11 @@
 from textblob import TextBlob
 from newspaper import Article
-
-url = "https://en.wikipedia.org/wiki/Muhammad_Ali"
-article = Article(url)
+import pandas as pd
+from matplotlib import pyplot as plt
 
 # Loading Data
+url = "https://en.wikipedia.org/wiki/Muhammad_Ali"
+article = Article(url)
 
 article.download()
 article.parse()
@@ -20,7 +21,7 @@ blob = TextBlob(text0)
 sentiment0 = blob.sentiment.polarity 
 print(f"\nSentiment : {sentiment0}")
 
-# loading self made txt data
+# loading self made txt files
 with open('negative.txt', 'r') as n:
     text = n.read()
 
@@ -32,9 +33,25 @@ print("\n__Through Texts__")
 # Cheaking and showing polarity -1 to 1, Sentiment for data from txt files
 
 blob = TextBlob(text)
-sentiment = blob.sentiment.polarity # -1 to 1
-print(f"\nSentiment 1 : {sentiment}")
+sentiment1 = blob.sentiment.polarity # -1 to 1
+print(f"\nSentiment 1 : {sentiment1}")
 
 blob = TextBlob(text2)
 sentiment2 = blob.sentiment.polarity 
 print(f"\nSentiment 2  : {sentiment2}\n")
+
+# taking the values in dic
+Sentiments = {
+    "Names" : ["sentiment0", "sentiment1", "sentiment2"],
+    "values" : [sentiment0, sentiment1, sentiment2]
+    }
+
+# ploting the graph of th sentiments with values
+df = pd.DataFrame(Sentiments)
+
+df.plot(x= "Names", y= "values", kind= "line", color = 'black')
+
+plt.title("Sentiments Text Analysis Graph")
+plt.xlabel("Names")
+plt.ylabel("values")
+plt.show()
